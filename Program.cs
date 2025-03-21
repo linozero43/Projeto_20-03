9186413ec17bf5872de3045ceea0ff934397a9bb
FIC <fic@info.local>
2025-03-20 21:56:11 -0300

    Projeto20-03

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] notas = new int[5];
+double soma = 0.00;
+double media = 0.00;
+for (int pos = 0; pos < notas.Length; pos++)
+{
+    Console.Write($"Digite a {pos+1} notas:");
+    notas[pos] = int.Parse(Console.ReadLine());
+    soma = notas[pos] + soma;
+}
+    Console.WriteLine("Notas do aluno:");
+for (int pos = 0;pos < notas.Length; pos++)
+{
+    Console.Write($"{notas[pos]} " );
+}
+media = soma / 5;
+
+Console.WriteLine();
+
+Console.WriteLine($"Media: {media}");
+if (media >= 7)
+{
+    Console.WriteLine("O aluno foi APROVADO!");
+}
+else
+{
+    Console.WriteLine("O aluno foi REPROVADO!");
+}
