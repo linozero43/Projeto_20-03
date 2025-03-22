fff1a86269f5f53042c64b49cbe6a1b2dd372331
FIC <fic@info.local>
2025-03-21 21:54:14 -0300

    ATT2103

--- Program.cs
+++ Program.cs
@@ -2,27 +2,79 @@
 int[] notas = new int[5];
 double soma = 0.00;
 double media = 0.00;
-for (int pos = 0; pos < notas.Length; pos++)
-{
-    Console.Write($"Digite a {pos+1} notas:");
-    notas[pos] = int.Parse(Console.ReadLine());
-    soma = notas[pos] + soma;
-}
-    Console.WriteLine("Notas do aluno:");
-for (int pos = 0;pos < notas.Length; pos++)
-{
-    Console.Write($"{notas[pos]} " );
-}
-media = soma / 5;
+int menu = 0;
 
-Console.WriteLine();
+do
+{
+    Console.WriteLine("===Gerenciador de Notas===");
+    Console.WriteLine("1-Cadastrar Notas");
+    Console.WriteLine("2-Listar notas");
+    Console.WriteLine("3-Estatisticas");
+    Console.WriteLine("4-Sair");
+    menu = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Media: {media}");
-if (media >= 7)
-{
-    Console.WriteLine("O aluno foi APROVADO!");
+    if (menu == 1)
+    {
+        try
+        {
+            for (int pos = 0; pos < notas.Length; pos++)
+            {
+                Console.Write($"Digite a {pos + 1}ª nota:");
+                notas[pos] = int.Parse(Console.ReadLine());
+                soma = notas[pos] + soma;
+                if (notas[pos] < 0 || notas[pos] > 10)
+                {
+                    throw new ArgumentOutOfRangeException("Número invalido");
+                }
+            }
+            Console.WriteLine("Notas do aluno:");
+            for (int pos = 0; pos < notas.Length; pos++)
+            {
+                Console.Write($"{notas[pos]} ");
+            }
+
+            media = soma / 5;
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Media: {media}");
+            if (media >= 7)
+            {
+                Console.WriteLine("O aluno foi APROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("O aluno foi REPROVADO!");
+            }
+        }
+        catch (FormatException ex1)
+        {
+            Console.WriteLine("ERRO: Formato INVÁLIDO");
+        }
+        catch (OverflowException ex1)
+        {
+            Console.WriteLine("ERRO: Número muito grande");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Número invalido");
+        }
+    }
+
+    if (menu == 2)
+    {
+        Console.WriteLine("Notas do aluno:");
+        for (int pos = 0; pos < notas.Length; pos++)
+        {
+            Console.Write($"{notas[pos]} ");
+        }
+    }
+
+    if(menu == 3)
+    {
+        Console.WriteLine($"A média atual do aluno é: {media}");
+    }
+
+    Console.WriteLine();
 }
-else
-{
-    Console.WriteLine("O aluno foi REPROVADO!");
-}
+while (menu != 4);
--- obj/Debug/net9.0/Projeto.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9186413ec17bf5872de3045ceea0ff934397a9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
